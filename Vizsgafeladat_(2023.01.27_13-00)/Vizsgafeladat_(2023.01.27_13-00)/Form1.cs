1795e915c2317f8e8847a0527006e9ad6e16746e
Gyorgy0 <37160183+Gyorgy0@users.noreply.github.com>
2023-03-06 19:38:54 +0100

    Add files via upload

---
+++ Vizsgafeladat_(2023.01.27_13-00)/Vizsgafeladat_(2023.01.27_13-00)/Form1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vizsgafeladat__2023._01._27_13_00_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int feherek_szama = 0;
+        private string[] szin = {"Black", "White"};
+        private Random rnd = new Random();
+        private int meret = 75;
+        private Button[,] b = new Button[5, 5];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    b[i, j] = new Button();
+                    this.Controls.Add(b[i,j]);
+                    b[i, j].Size = new Size(meret,meret);
+                    b[i, j].Location = new Point(10+j*meret+j,10+i*meret+i);
+                    b[i, j].Click += new EventHandler(Kattintas);
+                }
+            }
+            KockakKeverese();
+        }
+        private void KockakKeverese() 
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    b[i, j].BackColor = Color.FromName(szin[rnd.Next(2)]);
+                }
+            }
+            timer1.Enabled = true;
+        }
+
+        private void Kattintas(object sender, EventArgs e) 
+        {
+            Button btn = (Button)sender;
+            if (btn.BackColor == Color.Black)
+            {
+                btn.BackColor = Color.White;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (b[i, j].BackColor == Color.White)
+                        {
+                            feherek_szama++;
+                        }
+                        if (feherek_szama == 25)
+                        {
+                            timer1.Enabled = false;
+                            DialogResult valasz = MessageBox.Show("Hurrá, sikerült kifehéríteni! Szeretnél megint játszani?", "Gratulálunk!", MessageBoxButtons.OK);
+                            if (valasz == DialogResult.OK)
+                            {
+                                KockakKeverese();
+                            }
+                        }
+                    }
+                }
+                feherek_szama = 0;
+            }
+            else 
+            {
+                KockakKeverese();
+                feherek_szama = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            KockakKeverese();
+            feherek_szama = 0;
+        }
+    }
+}
---
+++ Vizsgafeladat_(2023.02.02_13-00)/Vizsgafeladat_(2023.02.02_13-00)/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vizsgafeladat__2023._02._02_13_00_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random rnd = new Random();
+        private Button[] btn = new Button[10];
+        private int meret = 75;
+        private int[] sargaalakpoz = new int[5];
+        private int[] kekalakpoz = new int[5];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Alakokgeneralasa();
+            Kockakgeneralasa();
+        }
+        private void Alakokgeneralasa() 
+        {
+            int szamlalo=0;
+            for (int i = 0; i < 5; i++)
+            {
+                sargaalakpoz[i] = rnd.Next(2);
+                kekalakpoz[i] = rnd.Next(2, 4);
+                if (3-kekalakpoz[i] == sargaalakpoz[i]) 
+                {
+                    szamlalo++;
+                }
+            }
+            int tomb=0;
+            while (szamlalo == 5)
+            {
+                kekalakpoz[tomb] = rnd.Next(2,4);
+                tomb++;
+                if (sargaalakpoz[tomb] != kekalakpoz[tomb])
+                {
+                    szamlalo = 0;
+                }
+                else 
+                {
+                    tomb = 0;
+                }
+            }
+        }
+        private void Kattintas(object sender, EventArgs e) 
+        {
+            Button b = (Button)sender;
+            int szamlalo = 0;
+            if (kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] == 2)
+            {
+                kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] = 3;
+                b.Location = new Point(20 + kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] * meret, 25 + ((Convert.ToInt32((b.Name).ToString()) - 5) * meret));
+            }
+            else if (kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] == 3) 
+            {
+                kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] = 2;
+                b.Location = new Point(20 + kekalakpoz[Convert.ToInt32((b.Name).ToString())-5] * meret, 25 + ((Convert.ToInt32((b.Name).ToString()) - 5) * meret));
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (sargaalakpoz[i] == 3 - kekalakpoz[i])
+                {
+                    szamlalo++;
+                }
+                if (sargaalakpoz[i] != 3 - kekalakpoz[i])
+                {
+                    szamlalo--;
+                }
+            }
+            if (szamlalo == 5)
+            {
+                MessageBox.Show("Sikerült kirakni!", "Gratulálok!", MessageBoxButtons.OK);
+                Alakokgeneralasa();
+                this.Controls.Clear();
+                Kockakgeneralasa();
+            }
+        }
+        private void Kockakgeneralasa() 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                btn[i] = new Button();
+                this.Controls.Add(btn[i]);
+                btn[i].Name = Convert.ToString(i);
+                btn[i].Size = new Size(meret, meret);
+                btn[i].Click += new EventHandler(Kattintas);
+                if (i > 4)
+                {
+                    btn[i].Location = new Point(20 + (kekalakpoz[i-5] * meret), 25 + ((i-5) * meret));
+                    btn[i].BackColor = Color.Blue;
+                }
+                else
+                {
+                    btn[i].Enabled = false;
+                    btn[i].Location = new Point(20 + (sargaalakpoz[i] * meret), 25 + (i * meret));
+                    btn[i].BackColor = Color.Orange;
+                }
+            }
+        }
+    }
+}
